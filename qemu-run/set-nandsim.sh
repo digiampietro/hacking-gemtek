d386f33eec68bc2d1d11ed4aa6f67925abcf1dfe
Valerio Di Giampietro <valerio@digiampietro.com>
2019-08-17 17:24:55 +0200

    Minor modifications to mips4kc-qemu_mipsel_malta_defconfig and set-nandsim.sh

--- qemu-run/set-nandsim.sh
+++ qemu-run/set-nandsim.sh
@@ -6,8 +6,10 @@
 # in multiple of 128Kb
 #
 #modprobe nandsim first_id_byte=0x20 second_id_byte=0xa1 third_id_byte=0x00 fourth_id_byte=0x15 parts=8,8,8,20,20,256,256,224,220
-modprobe nandsim first_id_byte=0xc8 second_id_byte=0xd1 third_id_byte=0x80 fourth_id_byte=0x95 fifth_id_byte=0x42 parts=8,8,8,20,20,256,256,224,220
-#
+echo "----> loading nadsim kernel module"
+modprobe nandsim first_id_byte=0xc8 second_id_byte=0xd1 third_id_byte=0x80 fourth_id_byte=0x95  parts=8,8,8,20,20,256,256,224,220
+
+echo "----> erasing emulated nand partitions"
 flash_erase /dev/mtd0 0   8   #   1,024   Bootloader
 flash_erase /dev/mtd1 0   8   #   1,024   Bootloader2
 flash_erase /dev/mtd2 0   8   #   1,024   Config
@@ -18,6 +20,7 @@ flash_erase /dev/mtd6 0 256   #  32,768   Kernel2
 flash_erase /dev/mtd7 0 224   #  28,672   Storage1
 flash_erase /dev/mtd8 0 220   #  28,160   Storage2
 #
+echo "----> loading emulatad nand partitions"
 nandwrite /dev/mtd0 01-bootloader.bin
 nandwrite /dev/mtd1 02-bootloader2.bin
 nandwrite /dev/mtd2 03-config.bin
@@ -28,5 +31,15 @@ nandwrite /dev/mtd6 07-kernel2.bin
 nandwrite /dev/mtd7 08-storage.bin
 nandwrite /dev/mtd8 09-storages.bin
 #
+echo "----> attaching UBI device and mounting file system"
+if [ ! -d /mnt/jffs2 ]
+then
+    mkdir /mnt/jffs2
+fi
+ubiattach -O 2048 -m 8 -d 8
+mount -tubifs /dev/ubi8_0 /mnt/jffs2
+echo "----> ubifs file system mounted on /mnt/jffs2"
+
+echo "if needed you copy and paste the following commands"
 echo "export PATH=$PATH:/mips-root/bin:/mips-root/sbin:/mips-root/usr/bin:/mips-root/usr/sbin"
 echo "export LD_LIBRARY_PATH=/lib:/usr/lib:/mips-root/lib:/mips-root/usr/lib"
